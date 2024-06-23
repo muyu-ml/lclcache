8cc85b8481530900026f956a438900ebe9e3e2dd
conglongli <liconglong@163.com>
2024-06-23 16:27:48 +0800

    feat: 增加strlen、exists、del、mget、mset处理

--- src/main/java/com/lcl/lclcache/LclCache.java
+++ src/main/java/com/lcl/lclcache/LclCache.java
@@ -1,7 +1,9 @@
 package com.lcl.lclcache;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * cache entries
@@ -20,4 +22,24 @@ public class LclCache {
         map.put(key, value);
     }
 
+    public int del(String...keys){
+        return keys == null ? 0 : (int)Arrays.stream(keys).map(map :: remove).filter(Objects :: nonNull).count();
+    }
+
+    public int exists(String...keys){
+        return keys == null ? 0 : (int)Arrays.stream(keys).map(map :: containsKey).filter(x -> x).count();
+    }
+
+    public String[] mget(String...keys){
+        return keys == null ? new String[0] : Arrays.stream(keys).map(map :: get).toArray(String[]::new);
+    }
+
+    public void mset(String[] keys, String[] values) {
+        if(keys == null || keys.length == 0){
+            return;
+        }
+        for(int i=0; i< keys.length; i++){
+            map.put(keys[i], values[i]);
+        }
+    }
 }
--- src/main/java/com/lcl/lclcache/core/LclCacheHandler.java
+++ src/main/java/com/lcl/lclcache/core/LclCacheHandler.java
@@ -22,6 +22,7 @@ public class LclCacheHandler extends SimpleChannelInboundHandler<String> {
 
     private static final String CRLF = "\r\n";
     private static final String STRING_PREFIX = "+";
+    private static final String BULK_PREFIX = "$";
     private static final String OK = "OK";
     private static final String INFO = "LclCache Server[v1.0.0], created by lcl" + CRLF
                                      + "Mock Redis Server at " + LocalDate.now() + " in Beijing" + CRLF;
@@ -69,11 +70,52 @@ public class LclCacheHandler extends SimpleChannelInboundHandler<String> {
             //    $2
             //            k1
             String value = CACHE.get(args[4]);
-            if(value == null) {
-                simpleString(ctx, null);
-            } else {
-                bulkString(ctx, value);
+            bulkString(ctx, value);
+        } else if("STRLEN".equals(cmd)) {
+            // strlen 收到的报文如下
+            //*2
+            //$6
+            //strlen
+            //$2
+            //k1
+            String value = CACHE.get(args[4]);
+            integer(ctx, value == null ? 0 : value.length());
+        } else if("DEL".equals(cmd)) {
+            // strlen 收到的报文如下（允许删除多个）
+            //*5,$3,del,$2,k1,$2,k2,$2,k3,$2,k4
+            int len = (args.length -3)/2;
+            String[] keys = new String[len];
+            for(int i=0;i<len; i++){
+                keys[i] = args[4+i*2];
             }
+            int del = CACHE.del(keys);
+            integer(ctx, del);
+        } else if("EXISTS".equals(cmd)) {
+            // EXISTS 收到的报文如下（允许删除多个）
+            //*2,$6,EXISTS,$2,k1,$2,k2,$2,k3,$2,k4
+            int len = (args.length -3)/2;
+            String[] keys = new String[len];
+            for(int i=0;i<len; i++){
+                keys[i] = args[4+i*2];
+            }
+            integer(ctx, CACHE.exists(keys));
+        } else if("MGET".equals(cmd)) {
+            int len = (args.length -3)/2;
+            String[] keys = new String[len];
+            for(int i=0;i<len; i++){
+                keys[i] = args[4+i*2];
+            }
+            array(ctx, CACHE.mget(keys));
+        } else if("MSET".equals(cmd)) {
+            int len = (args.length -3)/4;
+            String[] keys = new String[len];
+            String[] values = new String[len];
+            for(int i=0;i<len; i++){
+                keys[i] = args[4+i*4];
+                values[i] = args[6+i*4];
+            }
+            CACHE.mset(keys, values);
+            simpleString(ctx, OK);
         } else {
             simpleString(ctx, OK);
         }
@@ -81,9 +123,35 @@ public class LclCacheHandler extends SimpleChannelInboundHandler<String> {
 
 
 
+    private void array(ChannelHandlerContext ctx, String[] array){
+        StringBuilder sb = new StringBuilder();
+        if(array == null) {
+            sb.append("*-1").append(CRLF);
+        } else if (array.length == 0) {
+            sb.append("*0").append(CRLF);
+        } else {
+            sb.append("*" + array.length + CRLF);
+            for (int i=0;i< array.length;i++){
+                sb.append("$").append(array[i].length()).append(CRLF).append(array[i]).append(CRLF);
+            }
+        }
+        writeByteBuf(ctx, sb.toString());
+    }
+    private void integer(ChannelHandlerContext ctx, int i){
+        writeByteBuf(ctx, ":" + i + CRLF);
+    }
 
     private void bulkString(ChannelHandlerContext ctx, String content){
-        writeByteBuf(ctx, "$" + content.getBytes().length + CRLF + content + CRLF);
+        String ret;
+        // 对 null 、空字符串做单独处理
+        if(content == null){
+            ret = "-1";
+        } else if(content.isEmpty()){
+            ret = "0";
+        } else {
+            ret = content.getBytes().length + CRLF + content;
+        }
+        writeByteBuf(ctx, BULK_PREFIX + ret + CRLF);
     }
 
     private void simpleString(ChannelHandlerContext ctx, String content){
